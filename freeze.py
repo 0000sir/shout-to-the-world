737096bb999bf92e45bfec4ad172932416dc54b4
0000sir <0000sir@gmail.com>
2018-09-23 17:29:40 +0800

    freeze the world

---
+++ freeze.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import numpy as np
+import ctypes as C
+import cv2
+
+libmog = C.cdll.LoadLibrary('./libmog2000.so')
+
+#cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+#cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+
+def getfg(img):
+    (rows, cols) = (img.shape[0], img.shape[1])
+    res = np.zeros(dtype=np.uint8, shape=(rows, cols))
+    libmog.getfg(img.shape[0], img.shape[1],
+                       img.ctypes.data_as(C.POINTER(C.c_ubyte)),
+                       res.ctypes.data_as(C.POINTER(C.c_ubyte)))
+    return res
+
+
+def getbg(img):
+    (rows, cols) = (img.shape[0], img.shape[1])
+    res = np.zeros(dtype=np.uint8, shape=(rows, cols, 3))
+
+    libmog.getbg(rows, cols, res.ctypes.data_as(C.POINTER(C.c_ubyte)))
+    return res
+
+count = 0
+
+if __name__ == '__main__':
+    c = cv2.VideoCapture('./videos/1537694513755874.mp4')
+    total = int(c.get(cv2.CAP_PROP_FRAME_COUNT))
+    while 1:
+        _, f = c.read()
+        fg = getfg(f)
+        bg = getbg(f)
+        if count < total-10:
+            count += 1
+            continue
+        #cv2.imshow('f', f)
+        #cv2.imshow('fg', getfg(f))
+        #cv2.imshow('window', bg)
+        cv2.imwrite('freeze.jpg', bg)
+        exit(0)
+        #if cv2.waitKey(1) == 27:
+        #   exit(0)
